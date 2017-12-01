75b574e97011c5da4e1bd58c585585e6eacf136f
Oskar og. Grabiec <ograbiec316@studzpsb.local>
2017-12-01 15:09:50 +0100

    Hp
    Xd

---
+++ Assets/Hp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hp : MonoBehaviour
+{
+
+    private float maxHealth = 100;
+    private float currentHealth = 100;
+    
+    
+    
+    public Texture2D healthTexture;
+
+    private float barWidth;
+    private float barHeight;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+
+        barHeight = Screen.height * 0.02f;
+        barWidth = barHeight * 10.0f;
+
+    }
+
+
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(Screen.width - barWidth - 10,
+                                 Screen.height - barHeight - 10,
+                                 currentHealth * barWidth / maxHealth,
+                                 barHeight),
+                        healthTexture);
+    }
+}
+    

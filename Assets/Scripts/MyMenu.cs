34f9636dfc9f07581971818b46f58798bbe15419
Oskar og. Grabiec <ograbiec316@studzpsb.local>
2017-12-01 14:03:08 +0100

    Init
    Wgranie projektu menu z pen drive'a

---
+++ Assets/Scripts/MyMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+
+        //Application.LoadLevel("Level2");
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

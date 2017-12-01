d17169cb34651e7fcaf5ae8fa7e5c25ca32a545d
Oskar og. Grabiec <ograbiec316@studzpsb.local>
2017-12-01 14:51:45 +0100

    Strzelanie
    Strzelanie

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    
+    [Range(0,20)]
+    public int bulletSpeed;
+
+	void Update ()
+    {
+        transform.Translate(Vector3.one * Time.deltaTime * bulletSpeed);
+
+    }
+}
---
+++ Assets/Scripts/WeaponController_p.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController_p : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+
+    private int poolSize;
+    private int bulletCount;
+    public List<GameObject> bulletPool;
+
+
+	void Start ()
+    {
+        poolSize = 20;
+
+        for ( int i =0; i< poolSize; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.SetActive(false);
+            bulletPool.Add(bullet);
+        }
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) Fire();
+	}
+
+    private void Fire()
+    {
+        if (bulletCount < poolSize - 1)
+
+            Destroy(bullet, 2.0f);
+        {
+            bulletPool[bulletCount].transform.position = bulletSpawnPoint.position;
+            bulletPool[bulletCount].SetActive(true);
+            bulletCount++;
+        }
+
+        if (bulletCount == poolSize - 1)
+        {
+            bulletPool[bulletCount].transform.position = bulletSpawnPoint.position;
+            bulletPool[bulletCount].SetActive(true);
+            bulletCount = 0;
+        }
+    }
+
+}
